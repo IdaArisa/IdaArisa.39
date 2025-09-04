de57c83948014cb1257bf53d3a8cff3f2492c259
IdaArisa <arisa_ida@mail.toyota.co.jp>
2025-09-05 08:12:21 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+
+let hokkaidou = document.getElementById("hokkaidou-id");
+hokkaidou.addEventListener('click', function () {
+  window.open("file:///c%3A/Users/1632561/OneDrive%20-%20%E3%83%88%E3%83%A8%E3%82%BF%E8%87%AA%E5%8B%95%E8%BB%8A%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BE/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97/foundations/_lesson-Presentation/hokkaidou.html");
+}
+)
+
+let chubu = document.getElementById("chubu-id");
+chubu.addEventListener('click', function () {
+  window.open("file:///c%3A/Users/1632561/OneDrive%20-%20%E3%83%88%E3%83%A8%E3%82%BF%E8%87%AA%E5%8B%95%E8%BB%8A%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BE/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97/foundations/_lesson-Presentation/chubu.html");
+}
+)
+
+let kinki = document.getElementById("kinki-id");
+chubu.addEventListener('click', function () {
+}
+)
+
+let chugoku = document.getElementById("chugoku-id");
+chugoku.addEventListener('click', function () {
+  window.open("file:///c%3A/Users/1632561/OneDrive%20-%20%E3%83%88%E3%83%A8%E3%82%BF%E8%87%AA%E5%8B%95%E8%BB%8A%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BE/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97/foundations/_lesson-Presentation/chugoku.html")
+}
+)
+
+let shikoku = document.getElementById("shikoku-id");
+shikoku.addEventListener('click', function () {
+}
+)
+
+let kyusyu = document.getElementById("kyusyu-id");
+kyusyu.addEventListener('click', function () {
+}
+)
